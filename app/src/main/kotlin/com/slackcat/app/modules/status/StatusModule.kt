d47fd48d541560afecdc8a069bdf2bb4fc2ebb03
Nick Konecny <konecnyna@gmail.com>
2024-11-07 21:17:39 -0500

    [CHORE] Suspend invokes and updated status

--- app/src/main/kotlin/com/slackcat/app/modules/status/StatusModule.kt
+++ app/src/main/kotlin/com/slackcat/app/modules/status/StatusModule.kt
@@ -1,11 +1,9 @@
 package com.slackcat.app.modules.status
 
-import com.slackcat.app.SlackcatAppGraph.globalScope
 import com.slackcat.chat.models.IncomingChatMessage
 import com.slackcat.chat.models.OutgoingChatMessage
 import com.slackcat.models.SlackcatModule
 import com.slackcat.presentation.buildMessage
-import kotlinx.coroutines.launch
 
 class StatusModule : SlackcatModule() {
     private val statusClient = StatusClient()
