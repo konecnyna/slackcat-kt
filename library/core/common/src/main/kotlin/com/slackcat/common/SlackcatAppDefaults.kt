c166d0670c0d1d2791aeca474aa23a2ef60a10ba
Nick Konecny <konecnyna@gmail.com>
2024-11-08 09:11:45 -0500

    [COMMENT] added large version

--- library/core/common/src/main/kotlin/com/slackcat/common/SlackcatAppDefaults.kt
+++ library/core/common/src/main/kotlin/com/slackcat/common/SlackcatAppDefaults.kt
@@ -3,5 +3,7 @@ package com.slackcat.common
 object SlackcatAppDefaults {
     const val DEFAULT_BOT_NAME = "slackcat"
     const val DEFAULT_BOT_EMOJI_ICON = ":cat:"
+
+    // Large version: https://i.imgur.com/TZhQd7d.png
     const val DEFAULT_BOT_IMAGE_ICON = "https://i.imgur.com/edjjZUN.png"
 }
