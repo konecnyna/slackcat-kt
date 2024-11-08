ec468fcc07644fb91130c7b53b9287f54bfdd3f9
Nick Konecny <konecnyna@gmail.com>
2024-11-08 08:45:26 -0500

    [ICON] use slack emoji notation

--- library/core/common/src/main/kotlin/com/slackcat/common/SlackcatAppDefaults.kt
+++ library/core/common/src/main/kotlin/com/slackcat/common/SlackcatAppDefaults.kt
@@ -2,5 +2,5 @@ package com.slackcat.common
 
 object SlackcatAppDefaults {
     const val DEFAULT_BOT_NAME = "slackcat"
-    const val DEFAULT_BOT_EMOJI_ICON = "🐱"
+    const val DEFAULT_BOT_EMOJI_ICON = ":cat:"
 }
