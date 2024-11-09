3e6e00bbb74e722137953e91951e4d4090ef0973
JPoirier55 <jake.poirier55@gmail.com>
2024-11-09 13:22:12 -0500

    postgres and db agnostic

--- app/src/main/kotlin/com/slackcat/app/SlackcatApp.kt
+++ app/src/main/kotlin/com/slackcat/app/SlackcatApp.kt
@@ -10,6 +10,8 @@ import com.slackcat.app.modules.pokecat.PokeCatModule
 import com.slackcat.app.modules.status.StatusModule
 import com.slackcat.app.modules.translate.TranslateModule
 import com.slackcat.models.SlackcatModule
+import org.apache.commons.dbcp2.BasicDataSource
+import javax.sql.DataSource
 import kotlin.reflect.KClass
 
 class SlackcatApp {
@@ -25,11 +27,36 @@ class SlackcatApp {
             LearnModule::class,
         )
 
+    private fun createDataSource(): DataSource {
+        val databaseUrl = "${System.getenv("DATABASE_URL")?: "jdbc:sqlite:"}/${System.getenv("DATABASE_NAME")}"
+        val driverName = when {
+            databaseUrl.startsWith("jdbc:sqlite") -> "org.sqlite.JDBC"
+            databaseUrl.startsWith("jdbc:postgresql") -> "org.postgresql.Driver"
+            databaseUrl.startsWith("jdbc:mysql") -> "com.mysql.cj.jdbc.Driver"
+            else -> { throw IllegalArgumentException("Unsupported database URL: $databaseUrl") }
+        }
+        println("Database URL: $databaseUrl")
+        println("Driver: $driverName")
+        val dataSource = BasicDataSource().apply {
+            url = databaseUrl
+            username = System.getenv("DATABASE_USER") ?: ""
+            password = System.getenv("DATABASE_PASSWORD") ?: ""
+            driverClassName = driverName
+            maxTotal = 10  // Maximum number of connections in the pool
+            maxIdle = 5    // Maximum number of idle connections in the pool
+            minIdle = 2    // Minimum number of idle connections in the pool
+        }
+        println("Datasource: $dataSource")
+        return dataSource
+    }
+
     fun onCreate(args: String?) {
-        val slackcatBot = SlackcatBot(
-            modulesClasses = modules,
-            coroutineScope = SlackcatAppGraph.globalScope,
-        )
+        val slackcatBot =
+            SlackcatBot(
+                modulesClasses = modules,
+                coroutineScope = SlackcatAppGraph.globalScope,
+                databaseConfig = createDataSource()
+            )
         slackcatBot.start(args)
     }
 }
--- library/core/database/src/main/kotlin/com/slackcat/database/DatabaseClient.kt
+++ library/core/database/src/main/kotlin/com/slackcat/database/DatabaseClient.kt
@@ -4,14 +4,11 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.transaction
+import javax.sql.DataSource
 
 class DatabaseClient {
-    companion object {
-        val DATABASE_NAME = "slackcat-${System.getenv("ENV")?.lowercase() ?: "development"}"
-    }
-
-    fun initialize(storageClients: List<Table>) {
-        Database.connect("jdbc:sqlite:$DATABASE_NAME.db", driver = "org.sqlite.JDBC")
+    fun initialize(storageClients: List<Table>, databaseConfig: DataSource) {
+        Database.connect(databaseConfig)
         transaction {
             storageClients.forEach { SchemaUtils.create(it) }
         }
