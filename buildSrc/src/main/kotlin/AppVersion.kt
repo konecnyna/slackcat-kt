05ed71807a41fa8ecc3de8cafe3372e93b168d84
defkon <konecnyna@gmail.com>
2025-12-13 12:30:39 -0500

    Update PATCH version to 5

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -1,7 +1,7 @@
 object AppVersion {
     const val MAJOR = 0
     const val MINOR = 0
-    const val PATCH = 4
+    const val PATCH = 5
 
     val versionName: String
         get() = "$MAJOR.$MINOR.$PATCH"
