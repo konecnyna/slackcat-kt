c402f25b701845b06d2ef24227735dd0a4d8215e
defkon <konecnyna@gmail.com>
2025-12-14 13:24:03 -0500

    Update AppVersion.kt

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -1,7 +1,7 @@
 object AppVersion {
     const val MAJOR = 0
     const val MINOR = 0
-    const val PATCH = 10
+    const val PATCH = 11
 
     val versionName: String
         get() = "$MAJOR.$MINOR.$PATCH"
